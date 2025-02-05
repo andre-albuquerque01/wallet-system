1c813feaa4b3ac8e21bc867b61c80a3e7bdbfd50
andre <andrealbuquerque2001@gmail.com>
2025-02-05 12:18:48 -0300

    feat: update

--- Api/app/Http/Controllers/Api/TransactionController.php
+++ Api/app/Http/Controllers/Api/TransactionController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TransactionRequest;
+use App\Services\TransactionService;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    //
+    private $transactionService;
+    public function __construct(TransactionService $transactionService)
+    {
+        $this->transactionService = $transactionService;
+    }
+
+    public function index()
+    {
+        return $this->transactionService->index();
+    }
+
+    public function balance()
+    {
+        return $this->transactionService->balance();
+    }
+
+    public function show(string $id)
+    {
+        return $this->transactionService->show($id);
+    }
+
+    public function store(TransactionRequest $request)
+    {
+        return $this->transactionService->store($request->validated());
+    }
+
+    public function destroy(string $id)
+    {
+        return $this->transactionService->destroy($id);
+    }
 }
