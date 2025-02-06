e76c1bcf59687fd65b04c84961cc050829dbade6
andre <andrealbuquerque2001@gmail.com>
2025-02-06 12:42:01 -0300

    feat: create

---
+++ Api/app/Models/Transaction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory, HasUlids;
+
+    protected $table = "transactions";
+
+    protected $fillable = [
+        'value',
+        'type',
+        'sender_id',
+        'receiver_id',
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
