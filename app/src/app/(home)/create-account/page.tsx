a38fef3a9aca0eaeb268a15b02b390fd18bec7f6
andre <andrealbuquerque2001@gmail.com>
2025-02-13 09:16:09 -0300

    feat: create

---
+++ app/src/app/(home)/create-account/page.tsx
@@ -0,0 +1,9 @@
+import { CreateAccount } from "@/components/user/create-account";
+
+export default function Home() {
+  return (
+    <div className="h-full">
+      <CreateAccount />
+    </div>
+  );
+}
