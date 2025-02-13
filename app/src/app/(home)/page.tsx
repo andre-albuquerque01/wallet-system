41365539d35472ad473b5c49a85be4b009bbb394
andre <andrealbuquerque2001@gmail.com>
2025-02-13 09:16:12 -0300

    feat: create

---
+++ app/src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import { LoginComponent } from "@/components/user/login";
+
+export default function Login() {
+  return (
+    <div className="h-full">
+      <LoginComponent />
+    </div>
+  );
+}
