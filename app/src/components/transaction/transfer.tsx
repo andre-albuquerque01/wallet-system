4f9c05dfaec400845c92bba3e99be72ec3ec3fec
andre <andrealbuquerque2001@gmail.com>
2025-02-14 09:44:10 -0300

    feat: update

--- app/src/components/transaction/transfer.tsx
+++ app/src/components/transaction/transfer.tsx
@@ -1,6 +1,22 @@
+'use client'
+
+import { TransactionTransfer } from "@/app/action"
+import { useActionState, useEffect } from "react"
+
 export const TransferTransaction = () => {
+    const [state, action, pending] = useActionState(TransactionTransfer, {
+        ok: false,
+        error: ''
+    })
+
+    useEffect(() => {
+            if (state.ok) {
+                alert('Transferência feito com sucesso!')
+            }
+        }, [state])
+
     return (
-        <div className="mt-4 border-t pt-4">
+        <form action={action} className="mt-4 border-t pt-4">
             <h4 className="text-lg font-medium">Transferência</h4>
             <div className="grid grid-cols-2 gap-2">
                 <div>
@@ -8,20 +24,32 @@ export const TransferTransaction = () => {
                     <input
                         type="number"
                         className="border p-2 rounded w-full"
+                        name="value"
+                        required
+                        step="0.01"
                     />
                 </div>
                 <div>
                     <label className="block text-sm text-gray-600">Para quem</label>
                     <input
-                        type="text"
+                        type="email"
+                        name="email"
                         className="border p-2 rounded w-full"
+                        placeholder="Email do destinatário"
+                        required
                     />
                 </div>
             </div>
             <button
-                className="mt-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded w-full">
+                className="mt-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded w-full"
+                disabled={pending}>
                 Transferir
             </button>
-        </div>
+            {state.error && state.error && (
+                <span className="w-full max-md:w-80 flex flex-row items-center text-red-600 text-xs" aria-live="polite">
+                    {state.error && state.error}
+                </span>
+            )}
+        </form>
     )
 }
