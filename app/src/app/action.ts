a8bcfa623a83ddfc7d6a1325825e990448e7359b
andre <andrealbuquerque2001@gmail.com>
2025-02-17 09:15:08 -0300

    feat: update

--- app/src/app/action.ts
+++ app/src/app/action.ts
@@ -329,6 +329,26 @@ export async function GetWallerUser() {
     }
 }
 
+export async function GetBalanceWallerUser() {
+    const cookiesStore = await cookies()
+    try {
+        const response = await ApiServer('transactions/balance', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${cookiesStore.get('token')?.value}`,
+            },
+        })
+
+        const data = await response.json()
+
+        return data
+    } catch (error) {
+        return ApiError(error)
+    }
+}
+
 export async function TransactionCredit(state: { ok: boolean, error: string }, request: FormData) {
     const schema = z.object({
         type: z.string().default('credit'),
