7e17accba8b39510011c49332beaf90813794d5a
andre <andrealbuquerque2001@gmail.com>
2025-02-18 09:23:35 -0300

    feat: create

---
+++ app/src/app/(home)/user/(home)/page.tsx
@@ -0,0 +1,16 @@
+import { UserProfile } from "@/app/action";
+import { UserProfileComponent } from "@/components/user/profile";
+
+export default async function HomeUser() {
+    const data = await UserProfile()
+
+    return (
+        <div className="bg-forum-gradient-2 h-[calc(100vh-64px)] flex items-center justify-center overflow-hidden p-2">
+            <UserProfileComponent
+                name={data.name}
+                email={data.email}
+                nickname={data.nickname}
+            />
+        </div>
+    );
+}
