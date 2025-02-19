67e2583a93037fd872e3fefe40a6dbb26d0c621c
andre <andrealbuquerque2001@gmail.com>
2025-02-19 09:37:56 -0300

    feat: create

---
+++ app/src/app/(home)/recover-password/(index)/page.tsx
@@ -0,0 +1,9 @@
+import { RecoverPasswordComponent } from "@/components/user/recover-password";
+
+export default async function UpdateUser() {
+    return (
+        <div className="h-full">
+            <RecoverPasswordComponent />
+        </div>
+    )
+}
