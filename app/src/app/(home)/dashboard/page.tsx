c9420941a9ca84d5b28f06b1df97674e08f7fac8
andre <andrealbuquerque2001@gmail.com>
2025-02-20 09:37:14 -0300

    feat: update

--- app/src/app/(home)/dashboard/page.tsx
+++ app/src/app/(home)/dashboard/page.tsx
@@ -1,20 +1,28 @@
+import { GetBalanceWallerUser, GetWallerUser } from "@/app/action";
 import { CreditTransfer } from "@/components/transaction/credit";
 import { DebitTransfer } from "@/components/transaction/debit";
 import { HistoricComponent } from "@/components/transaction/historic";
 import { TransferTransaction } from "@/components/transaction/transfer";
+import Link from "next/link";
+import { FaRegUserCircle } from "react-icons/fa";
 
-export default function WalletDashboard() {
+export default async function WalletDashboard() {
+    const data = await GetBalanceWallerUser()
+    const historic = await GetWallerUser()
+
     return (
         <div className="min-h-screen flex flex-col p-6">
             <div className="w-full mx-auto">
-                <h2 className="text-2xl font-bold text-right">Saldo: R$ 2500</h2>
-
+                <div className="flex flex-row justify-between items-center">
+                    <Link href="/user"><FaRegUserCircle size={25} className="hover:text-blue-600" /></Link>
+                    <h2 className="text-2xl font-bold text-right">Saldo: R$ {data && data.balance.toFixed(2)}</h2>
+                </div>
                 <div className="mt-6">
                     <h3 className="text-xl font-semibold">Transações</h3>
                     <CreditTransfer />
                     <DebitTransfer />
                     <TransferTransaction />
-                    <HistoricComponent />
+                    <HistoricComponent data={historic} />
                 </div>
             </div>
         </div>
